cd9eeade509ad76746ddfa2d57167d48d6860df1
TusharPanchal55 <tpanchal827@gmail.com>
2025-07-17 23:32:01 +0530

    Fix: Set full PYTHON_VERSION in render.yaml

--- h_m/h_m/wsgi.py
+++ h_m/h_m/wsgi.py
@@ -7,6 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +15,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'h_m.settings')
 
 application = get_wsgi_application()
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
